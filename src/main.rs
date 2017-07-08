7006ca0f7cf63a8ab5c42e539c1c883e695a75a7
Fernando Mendez <fmendez@Fernandos-iMac.local>
2017-07-08 17:03:34 -0400

    Removed superfluous line

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use lib;
 fn main() {
 // Do nothing. `cargo test --lib` will be use to run all the unit test in the problems.
-        println!("{}", sum_multiples_of_a_and_b_below_z(3,5,1000));
 }
